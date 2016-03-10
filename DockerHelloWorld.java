7dbce7bd55b5c9cc62ac3da31bdc0371b0d60c26
karandsc <karan.dsc021@gmail.com>
2016-03-10 18:07:36 +0530

    Update code for Satya and Anikesh

--- DockerHelloWorld.java
+++ DockerHelloWorld.java
@@ -5,7 +5,7 @@ public static void main (String [] Args)
 {
 System.out.println("Hello World From Docker");
 System.out.println("Hello From Karan !!");
-
+System.out.println("Hello Satya , Anikesh !!");
 }
 
 }
