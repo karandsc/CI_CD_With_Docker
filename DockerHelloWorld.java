fb48a4be6cabf7f30e889e09c91b836db3f44574
karandsc <karan.dsc021@gmail.com>
2016-04-07 12:57:44 +0530

    Updated Class

--- DockerHelloWorld.java
+++ DockerHelloWorld.java
@@ -4,8 +4,6 @@ public class DockerHelloWorld
 public static void main (String [] Args)
 {
 System.out.println("Hello World From Docker");
-System.out.println("Hello From Karan !!");
-System.out.println("Hello Satya , Anikesh ,Karan!!");
 }
 
 }
